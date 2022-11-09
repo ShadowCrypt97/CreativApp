dbc3ad1d81e1e20c3f44350b4311bc5b8aad999b
ShadowCrypt97 <sergio970228@gmail.com>
2022-11-09 01:06:06 -0500

    pantalla login y funcionalidad cambio a pantalla de registro

--- app/src/main/java/com/proyecto/appejemplomascotas/LoginActivity.kt
+++ app/src/main/java/com/proyecto/appejemplomascotas/LoginActivity.kt
@@ -1,15 +1,26 @@
 package com.proyecto.appejemplomascotas
 
 import android.app.Activity
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.widget.Button
 import android.widget.RelativeLayout
+import com.proyecto.appejemplomascotas.databinding.ActivityLoginBinding
+import com.proyecto.appejemplomascotas.databinding.ActivityMainBinding
 
 class LoginActivity: Activity() {
-    lateinit var relativeLayout: RelativeLayout
-    lateinit var animationDrawable: AnimationDrawable
+
+    lateinit var binding: ActivityLoginBinding //Manejar los elementos de la vista
+    lateinit var btn_registro:Button
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        btn_registro = binding.btnRegistro
+        btn_registro.setOnClickListener{
+            startActivity(Intent(this,RegistrarUsuarioActivity::class.java))
+        }
     }
 }
